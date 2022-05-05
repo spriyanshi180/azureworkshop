1c5f046585b9a443110e91ece681f7204bff77de
spriyanshi180 <104897912+spriyanshi180@users.noreply.github.com>
2022-05-05 16:07:51 +0530

    Updated details in index.js
    Changed name, title and email address

--- portfolio-template/index.js
+++ portfolio-template/index.js
@@ -1,6 +1,6 @@
-const name = 'Your Name'
-const title = 'Your Title'
-const mail = 'Your Email'
+const name = 'Priyanshi Singh'
+const title = 'Programming Ethusiast'
+const mail = 'spriyanshi180@gmail.com'
 
 document.getElementById('title').innerText = name
 document.getElementById('name').innerText = name
